795dcd25c8d439feb33bb1e6deb156208cfc9b92
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-02-02 20:04:09 +0530

    [Guru] Welcome to Find Max using Generics

---
+++ FindMaxUsingGenerics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxUsingGenerics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to \"Find Max of three numbers using generics\" program");
+        }
+    }
+}
