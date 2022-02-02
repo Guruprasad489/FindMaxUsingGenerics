fe6da4240a7f811d6eb080c09ec7296f36a5697c
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-02-03 00:29:53 +0530

    [Guru] implemented program to find max of 3 strings -UC3

--- FindMaxUsingGenerics/Program.cs
+++ FindMaxUsingGenerics/Program.cs
@@ -22,13 +22,18 @@ namespace FindMaxUsingGenerics
             {
                 case 1:
                     int intnum1 = 66, intnum2 = 78, intnum3 = 56;
-                    int intmax = maximum.GetMaxOfThree(intnum1, intnum2, intnum3);
-                    Console.WriteLine("Maximum of three integers is: " + intmax);
+                    int intMax = maximum.GetMaxOfThree(intnum1, intnum2, intnum3);
+                    Console.WriteLine("Maximum of three integers is: " + intMax);
                     break;
                 case 2:
                     float floatnum1 = 66.25f, floatnum2 = 24.55f, floatnum3 = 56.78f;
-                    float floatmax = maximum.GetMaxOfThree(floatnum1, floatnum2, floatnum3);
-                    Console.WriteLine("Maximum of three integers is: " + floatmax);
+                    float floatMax = maximum.GetMaxOfThree(floatnum1, floatnum2, floatnum3);
+                    Console.WriteLine("Maximum of three floating values is: " + floatMax);
+                    break;
+                case 3:
+                    string str1 = "Apple", str2 = "Peach", str3 = "Banana";
+                    string stringMax = maximum.GetMaxOfThree(str1, str2, str3);
+                    Console.WriteLine("Maximum of three strings is: " + stringMax);
                     break;
                 default:
                     Console.WriteLine("Please choose the correct option");
